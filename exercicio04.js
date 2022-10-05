9726ac0d5871bc207294347dc4099ac695d6019d
Alexandre <95762582+mmuuttii@users.noreply.github.com>
2022-10-05 12:54:30 -0300

    Add files via upload

---
+++ exercicio04.js
@@ -0,0 +1,59 @@
+let estudantes = [ {
+    nome: 'DR.Direspect',
+    nota1:8,
+    nota2:9
+
+},
+
+{
+    nome: 'NoobMaster69',
+    nota1:5,
+    nota2:9
+
+},
+
+{
+    nome: 'Juliana',
+    nota1:3,
+    nota2:5
+
+},
+
+{
+    nome: 'Ingrid',
+    nota1:4.5,
+    nota2:7,
+
+},
+
+{
+    nome:'Naruto',
+    nota1:2,
+    nota2:4,
+},
+
+{
+    nome:'Kakashi',
+    nota1:9.9,
+    nota2:9.8,
+}
+]
+
+
+
+const media =function (n1,n2,nomeDoA) {
+    const media = ((n1 + n2) / 2).toFixed(2)
+    
+    if(media >= 7){
+        return(`a media do aluno (a) ${nomeDoA} é : ${media}  
+        Parabéns,${nomeDoA}! você foi aprovado(a) no concurso!`)
+    }else {
+        return(`a media do aluno (a) ${nomeDoA} é : ${media}
+        Não foi dessa vez,${nomeDoA}! Tente novamente!`)
+    }
+}
+
+estudantes.map((alunoo) =>{
+    alert(media(alunoo.nota1, alunoo.nota2, alunoo.nome)); });
+
+
